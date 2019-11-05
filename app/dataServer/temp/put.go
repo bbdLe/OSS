32099a4d69e764c9b252e3e7dbb344b88c0cc181
bbdLe <dalibaxiaoliba19@gmail.com>
2019-11-06 00:55:53 +0800

    数据服务器增加put接口

--- app/dataServer/temp/put.go
+++ app/dataServer/temp/put.go
@@ -1,9 +1,43 @@
 package temp
 
 import (
+	"OSS/app/dataServer/config"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"strings"
 )
 
 func put(w http.ResponseWriter, r *http.Request) {
+	uuid := strings.Split(r.URL.EscapedPath(), "/")[2]
+	uuidPath := path.Join(config.ServerCfg.Server.StoragePath, "temp", uuid)
+	info, err := readTempInfoFromFile(uuidPath)
+	if err != nil {
+		log.Println("readTempInfoFromFile failed :", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
+	uuidDataPath := uuidPath + ".dat"
+	stat, err := os.Stat(uuidDataPath)
+	if err != nil {
+		log.Println("open filed failed :", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	os.Remove(uuidPath)
+	if stat.Size() != info.Size {
+		log.Printf("except file size : %d, but size is %d\n", info.Size, stat.Size())
+		os.Remove(uuidDataPath)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	commitFile(uuidDataPath, info)
+}
+
+func commitFile(tempFile string, info *tempInfo) {
+	os.Rename(tempFile, path.Join(config.ServerCfg.Server.StoragePath, "objects", info.Name))
 }
